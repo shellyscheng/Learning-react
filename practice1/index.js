8d35aa001b792cd23a4579c4a3d32e618d323944
shellyscheng <shellysc13@gmail.com>
2020-05-28 18:54:55 -0400

    updates:

--- practice1/index.js
+++ practice1/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<p>Hello world!!!</p>, document.getElementById("root"))
+ReactDOM.render(<h1>Hello world!!!</h1>, document.getElementById("root"))
