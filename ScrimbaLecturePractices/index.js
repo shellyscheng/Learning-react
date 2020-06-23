afd16188691a6da5e3100d4731d90cc9f3201316
shellyscheng <shellysc13@gmail.com>
2020-06-22 22:56:33 -0400

    added JSX to JS

---
+++ ScrimbaLecturePractices/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >=12 && hours < 17) {
+        timeOfDay = "afternoon"
+    }
+    else {
+        timeofDay = "night"
+    }
+
+    return (
+        <h1>Good {timeOfDay}</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
