73be604fd7255a08bfb0e8d006ed34ca531d67f1
Shelly Cheng <shellyc@splunk.com>
2021-06-23 00:12:15 -0700

    added react hooks part2 usestate()

---
+++ ScrimbaLecturePractices/23_react_hooks_part2.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react"
+
+// Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
+// Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
+
+function App() {
+    const [count, setCount] = useState(0)
+    
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
